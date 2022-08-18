a36c34952393fb8c4154f05d3c1c861843e7433e
Emmanuel <emmanuel.redu.rezende@gmail.com>
2022-08-18 18:47:00 -0300

    requisito 7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
@@ -10,7 +11,9 @@ class App extends React.Component {
         <section>
           <SolarSystem />
         </section>
-        <section />
+        <section>
+          <Missions />
+        </section>
       </div>
     );
   }
